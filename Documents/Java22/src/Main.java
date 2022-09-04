c04f3619552570110816f96f557232bde418395e
Samuel Cox/sunsetworld <68568350+sunsetworld@users.noreply.github.com>
2022-09-04 09:23:58 +0100

    Some changes.

---
+++ Documents/Java22/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello. I'm Sam, I'm from England.");
+        System.out.println("This is a Java program.");
+    }
+}
---
+++ Documents/Java22/src/StudentProfile.java
@@ -0,0 +1,11 @@
+public class StudentProfile {
+    public static void main(String[] args) {
+        int StudentAge = 19;
+        double studentGPA = 3.45;
+        char firstName = 'S';
+        char lastName = 'C';
+        boolean perfectAttendance = true;
+
+
+    }
+}
